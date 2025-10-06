5b20418cff3476a66cc5e6d9ee6faec44f885b46
jadabrown822 <jadabrown822@gmail.com>
2025-10-06 17:29:48 -0500

    Dimming the PojectileLines with Each Shot

--- Assets/__Scripts/ProjectileLine.cs
+++ Assets/__Scripts/ProjectileLine.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 
 public class ProjectileLine : MonoBehaviour
 {
+    static List<ProjectileLine> PROJ_LINES = new List<ProjectileLine>();
+    private const float DIM_MULT = 0.75f;
+
     private LineRenderer _line;
     private bool _drawing = true;
     private Projectile _projectile;
@@ -18,7 +21,9 @@ public class ProjectileLine : MonoBehaviour
         _line.positionCount = 1;
         _line.SetPosition(0, transform.position);
 
-        _projectile = GetComponent<Projectile>();
+        _projectile = GetComponentInParent<Projectile>();
+
+        ADD_LINE(this);
     }
 
 
@@ -41,6 +46,29 @@ public class ProjectileLine : MonoBehaviour
         }
     }
 
+
+    private void OnDestroy()
+    {
+        // Remove this ProjectileLine from PROJ_LINES
+        PROJ_LINES.Remove(this);
+    }
+
+
+    static void ADD_LINE(ProjectileLine newLine)
+    {
+        Color col;
+
+        // Iterate over all the old lines and dim them
+        foreach (ProjectileLine pl in PROJ_LINES)
+        {
+            col = pl._line.startColor;
+            col = col * DIM_MULT;
+            pl._line.startColor = pl._line.endColor = col;
+        }
+
+        // Add newLine to the List
+        PROJ_LINES.Add(newLine);
+    }
 
     /*
         // Update is called once per frame
